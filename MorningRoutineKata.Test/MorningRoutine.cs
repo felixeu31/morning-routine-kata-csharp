571d35396bd0589719d45b07ca28c4e7833d356e
Félix Díez Arias <fdiez@tunstall.es>
2024-03-08 13:29:09 +0100

    green: no activity

--- MorningRoutineKata.Test/MorningRoutine.cs
+++ MorningRoutineKata.Test/MorningRoutine.cs
@@ -15,12 +15,15 @@ public class MorningRoutine : IMorningRoutine
     }
     public string WhatShouldIDoNow()
     {
+        if (_clock.Now().Hour == 6)
+            return "Do exercise";
+        
         if (_clock.Now().Hour == 7)
             return "Read and study";
      
         if (_clock.Now().Hour == 8)
             return "Have breakfast";
 
-        return "Do exercise";
+        return "No activity";
     }
 }
