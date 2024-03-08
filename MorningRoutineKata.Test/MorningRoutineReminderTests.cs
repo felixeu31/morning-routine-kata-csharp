0c1efbb48eb094ff01f0183b4097b92125a3e02c
Félix Díez Arias <fdiez@tunstall.es>
2024-03-08 12:50:13 +0100

    RED: remind_me_to_do_exercise_from_06_00_to_06_59

---
+++ MorningRoutineKata.Test/MorningRoutineReminderTests.cs
@@ -0,0 +1,31 @@
+namespace MorningRoutineKata.Test;
+using FluentAssertions;
+
+public class MorningRoutineReminderTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void remind_me_to_do_exercise_from_06_00_to_06_59()
+    {
+        // arrange
+        MorningRoutineReminder reminder = new();
+        
+        // act
+        string response = reminder.Remind();
+        
+        // assert
+        response.Should().Be("Do exercise");
+    }
+}
+
+public class MorningRoutineReminder
+{
+    public string Remind()
+    {
+        throw new NotImplementedException();
+    }
+}
