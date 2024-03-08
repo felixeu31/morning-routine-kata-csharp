88b5d900017c7a807bcb6df809047ead2ede21ef
Félix Díez Arias <fdiez@tunstall.es>
2024-03-08 17:16:16 +0100

    refactor: move configurations into separate collection

--- MorningRoutineKata.Test/MorningRoutine.cs
+++ MorningRoutineKata.Test/MorningRoutine.cs
@@ -8,22 +8,37 @@ public interface IMorningRoutine
 public class MorningRoutine : IMorningRoutine
 {
     private readonly IClock _clock;
+    private readonly IReadOnlyList<RoutineConfiguration> _configurations;
 
     public MorningRoutine(IClock clock)
     {
         _clock = clock;
+        _configurations = new List<RoutineConfiguration>()
+        {
+            new RoutineConfiguration(new TimeRange(TimeOnly.Parse("06:00:00"), TimeOnly.Parse("06:59:00")), "Do exercise"),
+            new RoutineConfiguration(new TimeRange(TimeOnly.Parse("07:00:00"), TimeOnly.Parse("07:59:00")), "Read and study"),
+            new RoutineConfiguration(new TimeRange(TimeOnly.Parse("08:00:00"), TimeOnly.Parse("08:59:00")), "Have breakfast")
+        };
     }
     public string WhatShouldIDoNow()
     {
-        if (_clock.Now().Hour == 6)
-            return "Do exercise";
+        var now = TimeOnly.FromDateTime(_clock.Now());
         
-        if (_clock.Now().Hour == 7)
-            return "Read and study";
-     
-        if (_clock.Now().Hour == 8)
-            return "Have breakfast";
+        var matchedConfiguration = _configurations.FirstOrDefault(x => x.TimeRange.IsWithin(now));
+
+        if (matchedConfiguration != null)
+            return matchedConfiguration.Name;
 
         return "No activity";
     }
 }
+
+public record TimeRange(TimeOnly From, TimeOnly To)
+{
+    public bool IsWithin(TimeOnly time)
+    {
+        return From <= time && time <= To;
+    }
+};
+
+public record RoutineConfiguration(TimeRange TimeRange, string Name);
