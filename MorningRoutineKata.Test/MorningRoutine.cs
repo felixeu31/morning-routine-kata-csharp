17b48f20fd9ca4e47836b82a1b6b86733cd2513a
Félix Díez Arias <fdiez@tunstall.es>
2024-03-09 18:20:32 +0100

    green: WithConfiguration_ForOverlappingTimes_ThrowsAnException

--- MorningRoutineKata.Test/MorningRoutine.cs
+++ MorningRoutineKata.Test/MorningRoutine.cs
@@ -18,12 +18,22 @@ public class MorningRoutine : IMorningRoutine
     
     public MorningRoutine WithConfiguration(TimeOnly from, TimeOnly to, string name)
     {
+        if (AnyExistingConfigurationOveralps(from, to))
+        {
+            throw new InvalidRoutineConfigurationException();
+        }
         _configurations.Add(new RoutineConfiguration(from, to, name));
         return this;
     }
+
     public string WhatShouldIDoNow()
     {
         return GetMatchingConfiguration(_clock.Now())?.Name ?? "No activity";
+    }
+
+    private bool AnyExistingConfigurationOveralps(TimeOnly from, TimeOnly to)
+    {
+        return _configurations.Any(existing => existing.IsWithin(from) || existing.IsWithin(to));
     }
 
     private RoutineConfiguration? GetMatchingConfiguration(TimeOnly now)
